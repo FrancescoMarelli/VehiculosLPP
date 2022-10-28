9df23c5780d7646ec23b325c684fb2425f7edda3
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-10-28 01:35:52 +0000

    Funcion coste con descuento

--- lib/vehiculo.rb
+++ lib/vehiculo.rb
@@ -14,3 +14,10 @@ end
 def tot_cost(days, price_pday)
     result = days * price_pday
 end
+
+#función que calcula el precio de alquiler con descuento aplicado
+def discount_price(tot_cost, percent)
+    percent = percent/100
+    discount = tot_cost * percent
+    p result = tot_cost - discount
+end
