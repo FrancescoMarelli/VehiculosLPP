f557ee313707d031aebc81dd7353af4a862febb4
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-11-03 17:09:04 +0000

    pruebas clase vacia

--- lib/vehiculo.rb
+++ lib/vehiculo.rb
@@ -1,5 +1,4 @@
 class Vehiculo
-    
 
 
 end
--- spec/vehiculo_spec.rb
+++ spec/vehiculo_spec.rb
@@ -1,7 +1,10 @@
 require 'vehiculo'
 
 describe Vehiculo do
-    it "Prueba constructor" do
-        expect(Vehiculo.new).to eq(nil);
+    it "Clase vacia" do
+        expect(Vehiculo.new).not_to eq(nil);
+        expect((Vehiculo.new).instance_of?Vehiculo)
+        expect((Vehiculo.new).is_a?Vehiculo).to eq(true)
+        expect((Vehiculo.new).class).to eq(Vehiculo)
     end
 end
