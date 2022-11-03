a125233a3c8f5a6cc884d81c1dd1777869468ec5
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-11-03 22:02:34 +0000

    Prueba Constructor realizada: Funciona

--- lib/vehiculo.rb
+++ lib/vehiculo.rb
@@ -1,4 +1,27 @@
 class Vehiculo
+    attr_reader :id, :fab, :year, :mpg, :price, :owner, :phone
+    def initialize(id, fab, year, mpg, price, hash)
+        @id = id
+        @fab = fab
+        @mpg = mpg
+        @price = price
+        @owner = hash[:owner]
+        @phone = hash[:phone]
 
+        if (year < 1900 || year > 2022) then 
+            puts " El año de fabricación tiene que ser comprendido entre 1900 y 2022"
+            else
+                 @year = year
+        end
+        
+    end
 
-end
+    def to_s
+        "(#{@id}, #{@fab}, #{@mpg}, #{@price}, #{@hash})"
+    end
+end 
+
+x = Hash[owner: "Francesco", phone: 3450903]
+v = Vehiculo.new(1,"Mercedes", 2000, 125, 2500, x)
+puts v.to_s
+
--- spec/vehiculo_spec.rb
+++ spec/vehiculo_spec.rb
@@ -1,10 +1,20 @@
 require 'vehiculo'
 
 describe Vehiculo do
-    it "Clase vacia" do
-        expect(Vehiculo.new).not_to eq(nil);
-        expect((Vehiculo.new).instance_of?Vehiculo)
-        expect((Vehiculo.new).is_a?Vehiculo).to eq(true)
-        expect((Vehiculo.new).class).to eq(Vehiculo)
+    before :all do
+        @x = Hash[owner: "Francesco", phone: 3450903]
+        @v = Vehiculo.new(1,"Mercedes", 2000, 125, 2500, @x)
     end
+    
+     #it "Clase vacia" do
+    #    expect(Vehiculo.new).to eq(nil);
+    #    expect((Vehiculo.new).instance_of?Vehiculo)
+    #    expect((Vehiculo.new).is_a?Vehiculo).to eq(true)
+    #    expect((Vehiculo.new).class).to eq(Vehiculo)
+    #end
+
+        it "Pruebas costructor" do
+             expect(Vehiculo.new(1, "Mercedes", 2000, 234, 2349, @x)).not_to eq(nil);
+        end
+
 end
