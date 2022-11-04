6c0b5886b436c2410ed54a0a3ff4e240f57e5cb3
FrancescoMarelli <alu0101161730@ull.edu.es>
2022-11-04 02:00:36 +0000

    Control errores costru

--- lib/vehiculo.rb
+++ lib/vehiculo.rb
@@ -2,12 +2,33 @@ class Vehiculo
     attr_reader :id, :fab, :year, :mpg, :price, :owner, :phone
     
     def initialize(id, fab, year, mpg, price, hash)
-        @id = id
-        @fab = fab
-        @mpg = mpg
-        @price = price
-        @owner = hash[:owner]
-        @phone = hash[:phone]
+
+        @owner = hash[:owner] #Insertar en tabla hash propietario del vehiculo y su numero de telefono
+        @phone = hash[:phone] 
+
+        # Control numero identificación vehiculo
+        if(id.is_a?Integer) then
+            @id = id
+        else puts "El numero de identificación tiene que ser un entero"
+        end
+
+        if(fab.is_a?String) then
+            @fab = fab
+        else puts "El nombre del fabricante tiene que ser una cadena de caracteres"
+        end
+
+        if(mpg.is_a?Numeric) then
+            @mpg = mpg
+        else puts "Las millas por galón son representadas por numeros, suele ser flotante"
+        end
+
+        if(price.is_a?Numeric) then 
+            if(price > 0 ) then 
+                @price = price
+            else puts "El precio del vehiculo no puede ser una cifra negativa"
+            end
+        else puts "El precio del vehiculo tiene que tener valor numerico, no de otro tipo"
+        end
 
         if (year < 1900 || year > 2022) then 
             puts " El año de fabricación tiene que ser comprendido entre 1900 y 2022"
@@ -18,11 +39,11 @@ class Vehiculo
     end
 
     def to_s
-        "(#{@id}, #{@fab}, #{@mpg}, #{@price}, #{@hash})"
+        "(#{@id}, #{@fab}, #{@mpg}, #{@price}, #{@hash}, #{@hash})"
     end
 end 
 
-x = Hash[owner: "Francesco", phone: 3450903]
+x = Hash[owner: "Francesco", phone: "a"]
 v = Vehiculo.new(1,"Mercedes", 2000, 125, 2500, x)
 puts v.to_s
 
